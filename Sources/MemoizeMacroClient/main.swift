1e19a944e033996d42c8deec369c2ef9bc08c7ec
tornikegomareli <gomarelidevelopment@gmail.com>
2024-06-25 00:25:43 +0400

    test executable target for macro

---
+++ Sources/MemoizeMacroClient/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//
+//
+//  Created by Tornike Gomareli on 25.06.24.
+//
+
+import Foundation
+import Memoize
+
+class MemoizeClass {
+  @Memoize
+  func test(numberOne: Int, numberTwo: Int) {
+    
+  }
+}
+
